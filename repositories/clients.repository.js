fa5aba889240c86259be70cd9e2621d3f9f6355a
Ivan Greve <ivangreve@gmail.com>
2020-06-15 18:42:16 -0300

    Repositories external services

---
+++ repositories/clients.repository.js
@@ -0,0 +1,30 @@
+let axios = require('axios');
+
+  
+let getUsers = async () => {
+    try {
+        let users = await axios.get('http://www.mocky.io/v2/5808862710000087232b75ac');
+        return users.data.clients;
+    }
+    catch{
+        return { error: "Error from external service"}
+    }
+}
+
+let getUserByEmail = async(email) => {
+    try {
+        let users = await axios.get('http://www.mocky.io/v2/5808862710000087232b75ac');
+        let filteredClient = users.data.clients.filter((client) => client.email == email)
+        
+        return filteredClient;
+        
+    }
+    catch{
+        return { success: false, message: "Error from external service"}
+    }
+
+}
+
+
+exports.getUsers = getUsers;
+exports.getUserByEmail = getUserByEmail;
