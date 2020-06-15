17cdcd49e1c102b65fa54b5f94d9c2caea5694e0
Ivan Greve <ivangreve@gmail.com>
2020-06-15 18:35:46 -0300

    Helpers and seting up server

---
+++ server.js
@@ -0,0 +1,22 @@
+﻿require('rootpath')();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const errorHandler = require('helpers/error-handler');
+const cors = require('cors');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// api routes
+app.use('/api/clients', require('./clients/clients.controller'));
+
+// global error handler
+app.use(errorHandler);
+
+
+const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
